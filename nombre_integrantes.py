2086522ba0f6ca806049a4aea6693c6ffbcebe9e
NetAcad DEVASC <changeme@example.com>
2025-07-10 21:31:02 +0000

    Primer commit: scripts de examen DRY7122

---
+++ nombre_integrantes.py
@@ -0,0 +1,7 @@
+# nombres_integrantes.py
+
+integrantes = ["Joseph Quintana", "Bairon Pinchera"]
+
+print("Lista de integrantes del grupo:")
+for integrante in integrantes:
+    print(f"- {integrante}")
---
+++ verificacion_vlan_extondida_o_normal.py
@@ -0,0 +1,10 @@
+# verificacion_vlan_extondida_o_normal
+
+vlan = int(input("Ingrese el número de VLAN: "))
+
+if 1 <= vlan <= 1005:
+    print("La VLAN ingresada está en el rango normal.")
+elif 1006 <= vlan <= 4094:
+    print("La VLAN ingresada está en el rango extendido.")
+else:
+    print("La VLAN ingresada no es válida.")
